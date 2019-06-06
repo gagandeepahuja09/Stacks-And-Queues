0daa4c7747a735848d425516e971794898ce47f3
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-06 18:25:40 +0530

    good question

--- InterviewBit/SlidingWindowMaximum.cpp
+++ InterviewBit/SlidingWindowMaximum.cpp
@@ -2,13 +2,15 @@ vector<int> Solution::slidingMaximum(const vector<int> &A, int B) {
     deque<int> dq;
     vector<int> ret;
     for(int i = 0; i < A.size(); i++) {
-        if(!dq.empty() && dq.front() == i - B)
-            dq.pop_front();
-        while(!dq.empty() && A[i] >= A[dq.back()])
+        while(!dq.empty() && A[dq.back()] <= A[i])
             dq.pop_back();
         dq.push_back(i);
-        if(i >= B - 1)
+        if(dq.front() <= i - B) {
+            dq.pop_front();
+        }
+        if(i >= B - 1) {
             ret.push_back(A[dq.front()]);
+        }
     }
     return ret;
 }
