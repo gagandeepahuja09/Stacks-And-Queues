dc1888ae6226e4266705d3955bb5c0d1e58b33f0
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-09 19:55:31 +0530

    easier approach

--- InterviewBit/LargestRectangleInHistorgam.cpp
+++ InterviewBit/LargestRectangleInHistorgam.cpp
@@ -1,18 +1,32 @@
 int Solution::largestRectangleArea(vector<int> &A) {
-    A.push_back(0);
     stack<int> stk;
-    int n = A.size();
-    int maxArea = 0, i = 0;
-    while(i < n) {
-        if(stk.empty() || A[i] >= A[stk.top()]) 
-            stk.push(i++);
-        else {
-            int currMax = stk.top();
+    vector<int> left(A.size(), -1);
+    vector<int> right(A.size(), A.size());
+    for(int i = 0; i < A.size(); i++) {
+        while(!stk.empty() && A[stk.top()] >= A[i]) {
             stk.pop();
-            maxArea = max(maxArea, A[currMax] * (stk.empty() ? i : 
-            i - 1 - stk.top()));
-        }    
+        }
+        if(!stk.empty()) {
+            left[i] = stk.top();
+        }
+        stk.push(i);
     }
-    return maxArea;
+    while(!stk.empty())
+        stk.pop();
+    for(int i = A.size() - 1; i >= 0; i--) {
+        while(!stk.empty() && A[stk.top()] >= A[i]) {
+            stk.pop();
+        }
+        if(!stk.empty()) {
+            right[i] = stk.top();
+        }
+        stk.push(i);
+    }
+    int maxRec = 0;
+    for(int i = 0; i < A.size(); i++) {
+        int rec = (right[i] - left[i] - 1) * A[i];
+        maxRec = max(maxRec, rec);
+    }
+    return maxRec;
 }
 
