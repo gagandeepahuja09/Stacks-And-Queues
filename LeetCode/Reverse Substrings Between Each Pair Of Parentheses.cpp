4ae865dc8e2a969d2e26c5431bf0ff6e33459cad
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-09-19 10:28:36 +0530

    very good optimization(avoiding duplicate work of repeated reverses)

--- LeetCode/Reverse Substrings Between Each Pair Of Parentheses.cpp
+++ LeetCode/Reverse Substrings Between Each Pair Of Parentheses.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     string reverseParentheses(string s) {
@@ -15,6 +16,37 @@ public:
             else {
                 ret += s[i];
             }
+        }
+        return ret;
+    }
+};
+*/
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        int n = s.size();
+        stack<int> stk;
+        vector<int> pair(n);
+        string ret;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') {
+                stk.push(i);
+            }
+            else if(s[i] == ')') {
+                int t = stk.top();
+                stk.pop();
+                pair[i] = t;
+                pair[t] = i;
+            }
+        }
+        for(int i = 0, d = 1; i < n; i += d) {
+            if(s[i] == '(' || s[i] == ')') {
+                i = pair[i];
+                d = -d;
+            }
+            else
+                ret += s[i];
         }
         return ret;
     }
